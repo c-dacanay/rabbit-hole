c83ea935866b2e9282a3226c215c931576cfebd4
Christina Dacanay <tina.dacanay@gmail.com>
2021-04-19 23:34:34 -0400

    images, glossary, new copy

--- src/script.js
+++ src/script.js
@@ -1,4 +1,16 @@
 smallNav = document.getElementById("nav");
+let cardInfo = [{
+  id: "lurk",
+  src: "src/img/incel.gif"
+}, {
+  id: "dark"
+}, {
+  id: "meme"
+}, {
+  id:"rue"
+}, {
+  id: "gloss"
+}] 
 
 var myScrollFunc = function() {
   var y = window.scrollY;
@@ -7,6 +19,30 @@ var myScrollFunc = function() {
   } else {
     smallNav.style.display = "none"
   }
+  changeDiv(y)
+
 };
+
+let t = 1;
+let changeDiv = function(y){
+  // for (let t in targetIds) {
+    let target = targetIds[t]
+    if (y > (target.offsetTop + target.offsetHeight)) {
+    console.log('scrolled past', target)
+    t++
+    console.log(t)
+  } else {
+    // t--
+    console.log(t)
+  }
+// }
+}
+
+
+targetIds = []
+for (i = 0; i < cardInfo.length; i++) {
+  let card = cardInfo[i].id
+  targetIds.push(document.getElementById(card))
+} 
 
 window.addEventListener("scroll", myScrollFunc);
